e8b6bc9f23989a4121d712f795b4e8d7105c6351
lucas-khattar <59715368+lucas-khattar@users.noreply.github.com>
2021-09-22 18:27:12 -0700

    9/22
    09(ip)

--- src/test/java/_08_mocking/models/DeliveryDriverTest.java
+++ src/test/java/_08_mocking/models/DeliveryDriverTest.java
@@ -12,37 +12,56 @@ import static org.mockito.Mockito.when;
 class DeliveryDriverTest {
 
     DeliveryDriver deliveryDriver;
-
+    
+    @Mock
+    Car car;
+    
+    @Mock
+    CellPhone cellPhone;
+    
+    String name;
+    
+    String phoneNumber;
+    
+    int octaneGrade;
+    
     @BeforeEach
     void setUp() {
-
+    	MockitoAnnotations.openMocks(this);
+    	deliveryDriver = new DeliveryDriver(name, car, cellPhone);
     }
 
     @Test
     void itShouldWasteTime() {
         //given
-
+    	boolean expectedTimeWaste = true;
         //when
-
+    	when(cellPhone.browseCatMemes()).thenReturn(true);
+    	boolean actualWasteTime = deliveryDriver.wasteTime();
         //then
+    	assertEquals(expectedTimeWaste, actualWasteTime);
     }
 
     @Test
     void itShouldRefuel() {
         //given
-
+    	boolean expectedRefil = true;
         //when
-
+    	when(car.fillTank(octaneGrade)).thenReturn(true);
+    	boolean actualRefil = deliveryDriver.refuel(octaneGrade);
         //then
+    	assertEquals(expectedRefil, actualRefil);
     }
 
     @Test
     void itShouldContactCustomer() {
         //given
-
+    	boolean expectedCall = true;
         //when
-
+    	when(cellPhone.call(phoneNumber)).thenReturn(true);
+    	boolean actualCall = deliveryDriver.contactCustomer(phoneNumber);
         //then
+    	assertEquals(expectedCall, actualCall);
     }
 
 }
